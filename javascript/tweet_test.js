2620fc346f12717c499463df9136485d4124443c
Kevin Lau <thekevlau@gmail.com>
2014-02-08 00:52:29 -0500

    progress thus far...still a whiles to go

--- javascript/tweet_test.js
+++ javascript/tweet_test.js
@@ -1,25 +1,30 @@
-var following = new Array(beaker, randomperson, hello);
+//the idea is to check if, of the people tagged in a given tweet, which of those people am I following
+
+
+var following = new Array("beaker", "randomperson", "hello");
 var tweet = 'Speaking of which, it would be hilarious if Twitter added a "@beaker would like to recall the tweet" option @kennwhite @kylemaxwell';
 //iterate through the tweet and when you find an "@" sign, record the name until you hit another space
 //store this in an array
 
+//first extract the "people" in the tweet
 var index = 0;
-var array = new Array();
-for (var i = 0; i < tweet.length; i++){
+var array = [];
+//this assumes that the variable i is the same within both loops....perhaps it is not the case
+var i = 0
+for (i; i < tweet.length; i++){
 	if (tweet.charAt(i) == '@') {
-		for (tweet; tweet.charAt(i) != " "; i++) {
-			var string = "";
+		console.log("FOUND AN AT SIGN");
+		console.log(i);
+		for (i; tweet.charAt(i) != ' '; ++i) {
+			var string;
 			string += tweet.charAt(i);
+			console.log(string);
 		}
-	console.log(string);
-	var array[index] = string;
-	index++;
+	array[index] = string;
+	index = index+1;
+	string = "";
 	}
 
 }
 
-console.log("The people tagged in this are: ");
-for (var i = 0; i<array.length; i++) {
-	console.log(array[i] + '\n');
-}
 
