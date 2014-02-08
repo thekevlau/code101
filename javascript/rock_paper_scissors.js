2f296330fe039e90f71b3026cf40d858b498a744
Kevin Lau <thekevlau@gmail.com>
2014-02-07 23:49:16 -0500

    rudamentary implementation done

--- javascript/rock_paper_scissors.js
+++ javascript/rock_paper_scissors.js
@@ -1,46 +1,93 @@
 //based on the CodeAcademy JS tutorial, but I've added more detail to the specified implementation.
 
 function getInput () {
-do {
-    userChoice = prompt("Rock, Paper or Scissors?");
+    do {
+        userChoice = prompt("Rock, Paper or Scissors?");
+    }
+    while (userChoice != "Rock" && userChoice != "Paper" && userChoice != "Scissors");
+    console.log("Your choice is: " + userChoice);
+    return userChoice;
 }
-while (userChoice != "Rock" && userChoice != "Paper" && userChoice != "Scissors");
-console.log("Your choice is: " + userChoice);
-return userChoice;
 
-}
-
-userChoice = getInput(userChoice);
-
-
-
-computerChoice = Math.random();
-console.log(computerChoice);
-if (computerChoice < 0.33) {
-    computerChoice = "Rock";
-}
-//an error here....else if implies the previous if condition, so the 0.33 is redundant 
-else if (0.33 < computerChoice < 0.67) {
-    computerChoice = "Paper";
-}
-else {
-    computerChoice = "Scissors";
+function getComp () {
+    computerChoice = Math.random();
+    if (computerChoice < 0.33) {
+        computerChoice = "Rock";
+    }
+    else if (computerChoice < 0.67) {
+        computerChoice = "Paper";
+    }
+    else {
+        computerChoice = "Scissors";
+    }
+    console.log("The computer chose: " + computerChoice);
+    return computerChoice;
 }
 
 
-function compare (userInput, compInput){
+function compare (userInput, compInput, score){
     if (userInput == "Rock") {
         if (compInput == "Rock") {
             console.log("Tie!");
-            result = 0;
         }
         
         if (compInput == "Paper") {
-            console.log("Lose!");
-            compScore += 1; 
+            console.log("You Lose!");
+            score.comp += 1; 
         }
 
-        if (compInput == )
+        if (compInput == "Scissors") {
+            console.log("You Win!");
+            score.user += 1;
+        }
+    }
+
+    if (userInput == "Paper") {
+        if (compInput == "Rock") {
+            console.log("You Win!");
+            score.user += 1;
+        }
+        
+        if (compInput == "Paper") {
+            console.log("Tie!");
+        }
+
+        if (compInput == "Scissors") {
+            console.log("You Lose!");
+            score.comp += 1;
+        }
+    }
+
+    if (userInput == "Scissors") {
+        if (compInput == "Rock") {
+            console.log("You Lose!");
+            score.comp += 1;
+        }
+        
+        if (compInput == "Paper") {
+            console.log("You Win!");
+            score.user += 1; 
+        }
+
+        if (compInput == "Scissors") {
+            console.log("Tie!");
+        }
     }
     
 }
+
+//this boolean does not work. why.
+//is the only way to pass something by "reference" to make it an object?
+//is there any way to "save" modifications done to a variable (predefined globally, even) within a function otherwise?
+var bool = {value : false};
+while (bool.value != true) {
+    var score = {user : 0, comp : 0};
+    userChoice = getInput();
+    computerChoice = getComp();
+    compare(userChoice, computerChoice, score);
+    console.log("Here is the score: " + '\n' + "The Computer has: " + score.comp + '\n' + "You have: " + score.user);
+
+    bool.value = prompt("Play Again? 0 for Yes, 1 for No");
+}
+
+
